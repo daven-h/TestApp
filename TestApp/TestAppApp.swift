35ba88c411645a66cbd895aeff695858b23808cf
Daven Hill <djbrill03@gmaill.com>
2022-08-27 21:14:53 -0400

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Daven Hill on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
